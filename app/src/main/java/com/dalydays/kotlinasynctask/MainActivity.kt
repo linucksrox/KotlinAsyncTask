bdf46961ec2991d771ddf7450a26a89ad615e6f4
linucksrox <ericdalyblog@dalydays.com>
2018-03-14 13:37:02 -0400

    Implement the most basic AsyncTask ever

--- app/src/main/java/com/dalydays/kotlinasynctask/MainActivity.kt
+++ app/src/main/java/com/dalydays/kotlinasynctask/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dalydays.kotlinasynctask
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        PrintMessageTask().execute()
+    }
+
+    class PrintMessageTask : AsyncTask<Void, Void, Void?>() {
+        override fun doInBackground(vararg p0: Void?): Void? {
+            println("printing this message from PrintMessageTask")
+            return null
+        }
     }
 }
