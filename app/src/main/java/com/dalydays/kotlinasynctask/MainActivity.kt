976b2d3f710d98b1ab0b84d0f0e99acac07ed585
linucksrox <ericdalyblog@dalydays.com>
2018-03-14 14:01:48 -0400

    Implement FizzBuzz in AsyncTask, and print on both background thread and main UI thread

--- app/src/main/java/com/dalydays/kotlinasynctask/MainActivity.kt
+++ app/src/main/java/com/dalydays/kotlinasynctask/MainActivity.kt
@@ -10,13 +10,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        PrintMessageTask().execute()
+        // execute AsyncTask, send in Int value as the limit to count up to
+        GetFizzBuzzTask().execute(100)
     }
 
-    class PrintMessageTask : AsyncTask<Void, Void, Void?>() {
-        override fun doInBackground(vararg p0: Void?): Void? {
-            println("printing this message from PrintMessageTask")
-            return null
+    class GetFizzBuzzTask : AsyncTask<Int, Void, String>() {
+
+        override fun doInBackground(vararg p0: Int?): String {
+            var result = ""
+            for (i in 1..p0[0]!!) {
+                var currentString = ""
+                if (i % 3 == 0)
+                    currentString += "fizz"
+                if (i % 5 == 0)
+                    currentString += "buzz"
+                if (currentString.isEmpty())
+                    currentString += i.toString()
+                result += currentString + ","
+            }
+            println("printing in a background thread")
+            println(result)
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            println("printing on the main UI thread")
+            println(result)
         }
     }
 }
